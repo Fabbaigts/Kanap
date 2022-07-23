3cf2b561277e72828a25188ae71723fbeb199d64
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-07-23 09:55:45 +0200

    creation de condition de mise au panier ( choisir une couleur et une quantité

--- Js/product.js
+++ Js/product.js
@@ -1,3 +1,6 @@
+
+
+
 //console.log(document.location) ///Affiche les options de windows.location
 
 let params = new URLSearchParams(document.location.search); //
@@ -11,12 +14,12 @@ document.addEventListener("DOMContentLoaded",affichageProduit())
 
 
 //----------------------------FONCTION D'AFFICHAGE DU PRODUIT SUR LA PAGE --------------------------
-   async function  affichageProduit()
-   {//récupération du produit concerné par l'affichage via l'APi ,grâce à son Id récupéré plus haut
+async function  affichageProduit()
+  {//récupération du produit concerné par l'affichage via l'APi ,grâce à son Id récupéré plus haut
     const res = await fetch("http://localhost:3000/api/products/"+idUrl)//attente d'une réponse de l'api concernant un id de produit.
     const product = await res.json(); //Attend d'avoir une réponse avant d'éxecuter le reste du code Si réponse ok de l'APi, donne la réponse en format Json.
     console.log(product)//Affiche la réponse dans la console
-    //------------------image-------------
+    //-------------------image-<img>-------------
     const imgProduit=document.getElementsByClassName("item__img")[0];//recupere div avec la class item_img
     const img=document.createElement("img");
 
@@ -34,16 +37,66 @@ document.addEventListener("DOMContentLoaded",affichageProduit())
     const description = document.getElementById("description");
     description.textContent= product.description;
     //-------------------Option-<option value>---------------
-    const option = document.getElementById("colors");
-    for (let i of product.colors){
-       let option = document.createElement("option");
-       colors.appendChild(option);
-       option.value = i;
-       option.text = i;
-       console.log(i)
-    }}
+    
+    for (let i of product.colors)
+      {
+         let option = document.createElement("option");
+         colors.appendChild(option);
+         option.value = i;
+         option.text = i;
+         console.log(i)
+         
+      }
+      /* ----------------------------------------------------------------------------*/
+      const AjoutPanier = document.getElementById("addToCart");
+      let quantite = document.getElementById("quantity").value;
+      console.log(quantite);
 
 
-    //--Récupération des données du choix client depuis l'affichage écran .
 
+
+
+      AjoutPanier.addEventListener('click',function()
+      {
+          const colorValue = document.getElementById("colors").value;
+          let quantite = document.getElementById("quantity").value;
+      
+         
+         
+
+          let valArticle = 
+         {
+            id: idUrl, // idUrl est une constant déclarée au début lors de la récupértation de l'Id
+            quantité: quantite, // Const quantity
+            couleur: colorValue
+         } 
+          panier = [];
+         // Si quantité <1 OU couleur != i alors renvoie un message "Merci de bien vouloir choisir une option de couleur Et un quantité"
+          if ((quantite < 1) || (colorValue === "")){console.log(quantite); console.log(colorValue);
+           alert("Merci de bien vouloir renseigner une option couleur Et une Quantité! ")}
+
+          else 
+          {
+            panier.push(valArticle);//rajoute un élément article au tableau panier[]
+            console.log("console.log de la variable article  push dans le tableau 'panier'",valArticle);
+            remplissagelocalStorage(valArticle)
+
+
+
+            console.log(valArticle.quantité);
+            console.log(valArticle.couleur);
+            console.log(colorValue);
+
+             
+            function remplissagelocalStorage(valArticle) 
+            { 
+             
+             /*Si le localstorage à déjà une clé produit "key" similaire..... if(----- = valArticle.id && couleur) {valArticle.quantité++} */ 
+              // Sinon :         
+            localStorage.setItem((idUrl),JSON.stringify(valArticle)); 
+              
+            }
+          }
+      })
+  }
 
