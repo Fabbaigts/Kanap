279e650d4bb320396b979e172c9faaa4257fc067
Fabp40 <104199929+Fabp40@users.noreply.github.com>
2022-08-20 11:55:02 +0200

    quelques ajouts textuels page index

--- Js/script.js
+++ Js/script.js
@@ -1,3 +1,7 @@
+//**********************************************************************
+//*** Interrogation de l'APi sur l'existance d'un fichier "products" ***
+//**********************************************************************
+
 fetch("http://localhost:3000/api/products") // Appel vers l'API pour la récupération des données du fichier produits
 //Alors, Si le fichier cible est trouvé, (res.ok) retourne les informations en format JSON.
 .then
@@ -8,26 +12,25 @@ fetch("http://localhost:3000/api/products") // Appel vers l'API pour la récupé
       }
     }
   )
-
-    /*Ce résultat json étant lui aussi une Promise,
-    nous le retournons et récupérons sa vraie valeur
-    dans la fonction then() suivante, ayant comme paramètre 'catalogue'*/
 .then
   (function (catalogue) 
     {
-      console.log(catalogue); //Affichage dans la console du contenu de JSON
-      affichageProd(catalogue)// Lance la fonction "affichageProd" permettant l'affichage des produits du fichier JSON
+      // Lance la fonction "affichageProd" permettant l'affichage des produits du fichier JSON
+      affichageProd(catalogue);
     }
   )
-    //Sinon affiche le message d'erreur dans la console et son type
+
 .catch
   (function (err) 
     {
     console.log("Une erreur est survenue" + err);
     }
   );
+  
+//*************************************************************** 
+//*** Fonction d'affichage du catalogue sur la page d'accueil ***
+//*************************************************************** 
 
-/* Fonction d'affichage du catalogue sur la page d'accueil 
 (appelée plus haut dans le .then du fetch)*/
 
 function affichageProd(catalogue)
@@ -35,24 +38,9 @@ function affichageProd(catalogue)
   /*boucle pour chaque instance du catalogue sans indice précis*/
   for (let i of catalogue)
   {
-      //Affiche dans la console le "nom, name" de chaque instance de champ détecté.
+      
     console.log("Nouvel article détecté dans le fichier JSON:  " + i.name);
-      /* Insertion dans le DOM de code HTML (innerHTML) de chaque article contenu dans le fichier JSON.
-      1- le lien <a> vers une Url écrite grâce à l'id du tableau et spécifique au produit.
-      2- l'image du produit grâce à la valeur $imageUrl et la description "alt" $altTxt.
-      3- Le TITRE du produit (en H3) grâce à $name.
-      4- La description du produit dans un <p> de classe productDecription grâce à $description
-      5-  la valeur $Price du produit dans un <p> de class "productPrice"
-      */
-   /*  newArticle.innerHTML += 
-      `<a href="./product.html?_id=${i._id}"> 
-        <article>
-          <img src="${i.imageUrl}" alt="${i.altTxt}">
-          <h3 class="productName">${i.name}</h3>
-          <p class="productDescription">${i.description}</p>
-          <p class="productPrice">à partir de ${i.price}€</p>
-        </article>
-      </a>`;*/
+      
       const lien=document.createElement('a');
        lien.href=`./product.html?_id=${i._id}`;
 
